9ce1b314131f1f5457266dd9f5036ca42bd0b6eb
cortazar <rebeca.cortazar@deusto.es>
2017-03-27 15:45:39 +0200

    Initial Commit

---
+++ GameAdventureStrategyMaven/src/main/java/game/Barbarian.java
@@ -0,0 +1,24 @@
+package game;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Barbarian extends Character {
+	final Logger logger = LoggerFactory.getLogger(Barbarian.class);
+	
+	/**
+	* Constructor of Barbarian thar receives as argument a WeaponBehaviour.
+	*
+	* @param w Weapon with which the Barbarian will fight
+	*/
+	public Barbarian(WeaponBehaviour w) {
+		super(w);
+	}
+	
+	@Override
+	public void display() {
+	// TODO Auto-generated method stub
+		logger.info("Modified from B!");
+		logger.info("You are a strong, hulky barbarian!!!.");
+	}
+}
