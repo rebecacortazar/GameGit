5ee8cb6d964525b636f4ebb12a45d52464162e02
rebecacortazar <rebeca.cortazar@deusto.es>
2018-03-20 16:35:09 +0100

    User 2 intenta provocar conflicto

--- GameAdventureStrategyMaven/src/main/java/game/Paladin.java
+++ GameAdventureStrategyMaven/src/main/java/game/Paladin.java
@@ -11,6 +11,7 @@ public class Paladin extends Character {
 	// TODO Auto-generated method stub
 		System.out.println("You are a holy paladin, slayer of evil.");
 		System.out.println("User 2. commit 1");
-		System.out.println("User 2. commit 2");
+		System.out.println("User 2. commit 2. User 2 escribe en la misma linea de código");
+		System.out.println("Además User 2 añade código para forzar conflicto");
 	}
 }
