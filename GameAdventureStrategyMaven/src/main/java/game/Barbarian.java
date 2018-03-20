9b75b9563e23d6fc43dd8b5d8498a76660dd8f51
RCG <rebecacortazar@users.noreply.github.com>
2018-03-20 20:35:30 +0100

    Update Barbarian.java

--- GameAdventureStrategyMaven/src/main/java/game/Barbarian.java
+++ GameAdventureStrategyMaven/src/main/java/game/Barbarian.java
@@ -21,7 +21,7 @@ public class Barbarian extends Character {
 	public void display() {
 	// TODO Auto-generated method stub
 		logger.info("Barbarian");
-		logger.info("Barbarian cambios para conflicto en Eclipse");
+		logger.info("Provocando cambios directamente en Github para conflicto en Eclipse");
 	}
 		
 	}
